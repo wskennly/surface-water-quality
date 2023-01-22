72971a3a1d9ac3b9ea41d8d7aa40539d79a32cbb
Hubert <HubertMajewski@users.noreply.github.com>
2023-01-21 21:56:55 -0500

    Present basic plotly graph

--- NodeJS/src/App.js
+++ NodeJS/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Plot from 'react-plotly.js';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App-header" >
+			<Plot
+				data={[{
+							x: [1, 2, 3],
+							y: [2, 6, 3],
+							type: 'scatter',
+							mode: 'lines+markers',
+							marker: {color: 'red'},
+						},
+						{type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+					]}
+				layout={ {width: '100%', height: '100%', title: 'A Fancy Plot'} }
+				/>
+		</div>
+		);
 }
 
 export default App;
