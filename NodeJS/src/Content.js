a1dc271e7b8b8bceb148545e0707ae8314c6e57c
Hubert <HubertMajewski@users.noreply.github.com>
2023-01-29 21:21:04 -0500

    Move card generation. Fix styling

--- NodeJS/src/Content.js
+++ NodeJS/src/Content.js
@@ -1,10 +1,4 @@
-import { useEffect } from 'react';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Paper from '@mui/material/Paper';
-import Grid from '@mui/material/Grid';
-import Plot from "react-plotly.js"
+import { AppBar, Toolbar, Typography, Paper, Grid } from '@mui/material';
 import { usePageContext } from './PageContext';
 
 export const Card = ({ heading, children }) => (
@@ -40,39 +34,9 @@ export const Card = ({ heading, children }) => (
 export default function Content() {
   const [state, dispatch] = usePageContext();
 
-  useEffect(
-    //Mount
-    () => {
-      dispatch({
-        type: "addCard", value: [
-
-          //Ideally call a function to update the content than write all cards here
-          <Card heading={"Data"}>
-
-            <Plot
-              data={[{
-                x: [1, 2, 3],
-                y: [2, 6, 3],
-                type: 'scatter',
-                mode: 'lines+markers',
-                marker: { color: 'red' },
-              },
-              { type: 'bar', x: [1, 2, 3], y: [2, 5, 3] },
-              ]}
-              layout={{ width: '100%', height: '100%', title: 'A Fancy Plot' }}
-            />
-
-          </Card>
-        ]
-      })
-
-      //Unmount
-      return () => dispatch({ type: "clearCards" })
-    }, []);
-
   return (
     <Grid container direction="column"
-      sx={{ height: "100vh", overflow: 'hidden' }}
+      sx={{ py: 3, overflow: 'hidden' }}
       spacing={2}
       alignContent="center"
     >
