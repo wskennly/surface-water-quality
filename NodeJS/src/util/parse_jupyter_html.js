c76eb6d280a566507c272fedc4ede913162d5460
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-05 00:20:40 -0500

    Split HTML into heading and bosy

--- NodeJS/src/util/parse_jupyter_html.js
+++ NodeJS/src/util/parse_jupyter_html.js
@@ -1,20 +1,24 @@
 import axios from "axios";
-import { createRoot } from "react-dom";
 
 export const getTitleBodyPairs = fileName =>
 	axios.get("./" + "jupyter_html/" + fileName).then(response => {
 
 		const doc = new DOMParser().parseFromString(response.data, "text/html");
 
-		var titles = [...doc.querySelectorAll(".row.header")];
+		var Cell = [...doc.getElementsByClassName("jp-Cell")];
 
-		var body = [...doc.querySelectorAll(".section-items")];
+		return Cell.map((t, idx) => {
 
-		return titles.map((t, idx) => (
-				{
-					title: t,
-					body: body[idx],
-					style: null
-				}
-			));
+			var headding = [...t.getElementsByClassName("jp-InputArea")][0]
+			var body = [...t.getElementsByClassName("jp-OutputArea")][0]
+
+			return ({
+				title: headding,
+				body: cleanupCell(body)
+			})
+		});
 	});
+
+export const cleanupCell = cell => {
+	return cell;
+}
