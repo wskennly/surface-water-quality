aef5535295cfee0d6bb100c50650525100d0ec51
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-05 00:19:39 -0500

    Formatting

--- NodeJS/src/PageContext.js
+++ NodeJS/src/PageContext.js
@@ -1,6 +1,5 @@
 
-import { Suspense, createContext, cloneElement, useContext, useReducer, useEffect } from "react";
-import Plot from "react-plotly.js"
+import { createContext, useContext, useReducer, useEffect } from "react";
 import { Card } from "./Content";
 import { getTitleBodyPairs } from "./util/parse_jupyter_html";
 export const INIT_STATE = {
@@ -14,21 +13,21 @@ export const INIT_STATE = {
 export const pageReducer = (state, action) => {
 	switch (action.type) {
 		case "heading":
-			return {...state, heading: action.value}
+			return { ...state, heading: action.value }
 		case "subheading":
-			return {...state, subheadding: action.value}
+			return { ...state, subheadding: action.value }
 		case "selectedNavigation":
-			return {...state, selectedNavigation: action.value}
+			return { ...state, selectedNavigation: action.value }
 		case "selectedSubnavigation":
-			return {...state, selectedSubnavigation: action.value}
+			return { ...state, selectedSubnavigation: action.value }
 		case "addCards":
 			Array.isArray(action.value) ?
 				action.value.map(elm =>
 					state.visibleCards.push(elm)
 				) : state.visibleCards.push(action.value);
-			return {...state}
+			return { ...state }
 		case "clearCards":
-			return {...state, visibleCards: []}
+			return { ...state, visibleCards: [] }
 		default:
 			throw "Illegal Type for Reducer";
 	}
@@ -38,37 +37,39 @@ export const PageContext = createContext(INIT_STATE);
 export const usePageContext = () => useContext(PageContext);
 export const PageContextProvider = ({ children }) => {
 	const [pageState, pageDispatch] = useReducer(pageReducer, INIT_STATE);
+
 	useEffect(
 		//Mount
-			() => {
+		() => {
 
-			const pairs = getTitleBodyPairs("Full_table_interpolate.html");
+			const pairs = getTitleBodyPairs("Surfacewater_XGB.html");
+
 			pairs.then(pairs => {
+
 				pageDispatch({
 					type: "addCards", value: [
 						...pairs.map(p => {
 
-							//Ideally call a function to update the content than write all cards here
 							return (
-									<Card heading={<div dangerouslySetInnerHTML={{__html: p.title.outerHTML}} />} >
-										<div dangerouslySetInnerHTML={{__html: p.body.outerHTML}} style={{padding: "10px"}}/>
-									</Card>
+								<Card align="left" heading={<div dangerouslySetInnerHTML={{ __html: p.title.outerHTML }} />} >
+									<div dangerouslySetInnerHTML={{ __html: p.body?.outerHTML }} />
+								</Card>
 							)
-						})
+						}),
 					]
 				})
 			})
 
 			//Unmount
-			return () => pageDispatch({type: "clearCards"})
+			return () => pageDispatch({ type: "clearCards" })
 		}, []);
 
 	return (
 		<PageContext.Provider
 			value={[pageState, pageDispatch]}
-			>
+		>
 			{children}
 		</PageContext.Provider>
-		)
+	)
 
 }
