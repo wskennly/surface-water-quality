7ccfa72d06034b3489898de284b8684086f46940
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-09 17:37:32 -0500

    Proof of concept for creating pages based off navigation. Resolved issue with notebook script not running

--- NodeJS/src/pages/data-database.js
+++ NodeJS/src/pages/data-database.js
@@ -1,18 +1,19 @@
 import { useState, useEffect } from 'react';
 import { Grid } from '@mui/material';
 import Card from '../components/Card';
-import { getTitleBodyPairs } from '../util/parse_jupyter_html';
+import { getJupyterNotebookTitleBodyPairs, getPandasProfileReportTitleBodyPairs } from '../util/parse_jupyter_html';
 import { CircularProgress } from '@mui/material';
 
 export default () => {
+    const [cards, setCards] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [data, setData] = useState([]);
 
     //Set the data async
     useEffect(() =>
-        getTitleBodyPairs("Surfacewater_XGB.html")
+        getJupyterNotebookTitleBodyPairs("Surfacewater_XGB.html")
+        // getPandasProfileReportTitleBodyPairs("Full_table_interpolate.html")
             .then(res => {
-                setData(res)
+                setCards(res)
                 setLoading(false);
             })
         , [])
@@ -30,7 +31,7 @@ export default () => {
                     :
                     // Design your page from here
                     <>
-                        {data.map(p => (
+                        {cards.map(p => (
                             <Grid item style={{ width: "100%" }}>
                                 <Card align="left" heading={p.title} >
                                     {p.body}
