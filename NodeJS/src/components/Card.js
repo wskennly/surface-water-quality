fb8ffdbe70cab075e51a559c7f787a62cfcbb767
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-09 01:21:55 -0500

    Remove logic out of card into the util function

--- NodeJS/src/components/Card.js
+++ NodeJS/src/components/Card.js
@@ -1,35 +1,37 @@
 import { AppBar, Paper, Grid } from '@mui/material';
 
-export const Card = ({ heading, align, children }) => (
-  <Paper>
+export const Card = ({ heading, children }) => {
+
+  //Treat as array
+  children = (Array.isArray(children) ? children : [children]);
+
+  return <Paper>
 
     <AppBar
       position="static"
       color="default"
       elevation={0}
-      sx={{ borderRadius: 1, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderBottom: '1px solid rgba(0, 0, 0, 0.12)', overflowX: "auto" }}
+      sx={{ borderRadius: 1, borderBottom: '1px solid rgba(0, 0, 0, 0.12)', overflowX: "auto", ...(children[0] && children.length > 0 ? { borderBottomLeftRadius: 0, borderBottomRightRadius: 0 } : {}) }}
     >
-      {/* <Toolbar> */}
-        <Grid container>
-          <Grid item xs sx={{m : 1}}>
-            {/* <Typography color="text.secondary" align={align ?? "center"}> */}
-              {heading}
-            {/* </Typography> */}
-          </Grid>
+      <Grid container>
+        <Grid item xs sx={{ m: 1 }}>
+          {heading}
         </Grid>
-      {/* </Toolbar> */}
+      </Grid>
     </AppBar>
 
-    <Grid container >
-      {(Array.isArray(children) ? children : [children]).map(
-        child =>
-          <Grid item sx={{ m: 1, overflowX: "auto" }}>
+    <Grid container>
+      {children.map(
+        child => (child
+          ? <Grid item sx={{ width: "100%", m: 1, overflowX: "auto" }}>
             {child}
           </Grid>
+          : null
+        )
       )}
     </Grid>
 
-  </Paper>
-)
+  </Paper >
+}
 
 export default Card;
