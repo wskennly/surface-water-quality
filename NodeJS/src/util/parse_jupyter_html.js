a6e74762a70981e7536f9bbdf1bc9994b0419d69
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-09 01:22:13 -0500

    Remove logic out of card into the util function

--- NodeJS/src/util/parse_jupyter_html.js
+++ NodeJS/src/util/parse_jupyter_html.js
@@ -9,12 +9,13 @@ export const getTitleBodyPairs = fileName =>
 
 		return Cell.map((t, idx) => {
 
-			var headding = [...t.getElementsByClassName("jp-InputArea")][0];
-			var body = [...t.getElementsByClassName("jp-OutputArea")][0];
+			var headding = cleanupCell([...t.getElementsByClassName("jp-InputArea")][0], idx);
+			var body = cleanupCell([...t.getElementsByClassName("jp-OutputArea")][0], null);
 
+			//Convert to JSX
 			return ({
-				title: cleanupCell(headding, idx),
-				body: cleanupCell(body)
+				title: headding ? <div dangerouslySetInnerHTML={{__html: headding.innerHTML}} /> : null,
+				body: body ? <div dangerouslySetInnerHTML={{__html: body.innerHTML}} /> : null
 			})
 		});
 	});
@@ -23,14 +24,14 @@ export const cleanupCell = (cell, idx) => {
 
 	if (cell == null) return null;
 
-	//Process indivisually
+	//Process individually
 	if (Array.isArray(cell))
 		return cell.map(c => cleanupCell(c))
 
 	//Remove number from Notebook
 	const elements = [...cell.getElementsByClassName("jp-InputPrompt"), ...cell.getElementsByClassName("jp-OutputPrompt")]
 	elements.map(e => {
-		if (idx) e.parentNode.insertBefore(document.createTextNode(idx), e);
+		if (idx !== null) e.parentNode.insertBefore(document.createTextNode(idx + ": "), e);
 		e.parentNode.removeChild(e);
 	})
 
