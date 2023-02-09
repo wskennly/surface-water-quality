50c9e568bf1f083b028cdf5db0b27637814915c0
Hubert <HubertMajewski@users.noreply.github.com>
2023-02-08 23:55:18 -0500

    Remove config - not needed yet

--- NodeJS/src/config.js
+++
@@ -1,8 +0,0 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
- */
-
-export const baseURL = ""
-
-
